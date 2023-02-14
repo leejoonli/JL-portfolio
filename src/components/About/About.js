adbe369d0b12b98a844892deef4f34a27bfa7413
Jeremiah Lee <jhwlee94@gmail.com>
2023-02-13 19:59:59 -0800

    update About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ function About() {
         <div className={styles.aboutContainer} id='about'>
             <img src={profile} alt='Portrait of Jeremiah' className={styles.portrait} />
             <h1 className={styles.aboutHeader}>ABOUT ME</h1>
-            <p className={styles.aboutDesc}>Technology is ever-growing and advancing in our world. I strive to incorporate accessibility, reliability, and improved user experience.</p>
+            <p className={styles.aboutDesc}>Technology is ever-growing and advancing in our world. Striving to incorporate: accessibility, reliable applications, and improved user experience.</p>
         </div>
     );
 }

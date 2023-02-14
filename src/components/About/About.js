b44ae3cab801a904e60c7349df6aa637da6e2351
Jeremiah Lee <jhwlee94@gmail.com>
2023-02-13 21:07:32 -0800

    update About page and resume file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ function About() {
         <div className={styles.aboutContainer} id='about'>
             <img src={profile} alt='Portrait of Jeremiah' className={styles.portrait} />
             <h1 className={styles.aboutHeader}>ABOUT ME</h1>
-            <p className={styles.aboutDesc}>Technology is ever-growing and advancing in our world. Striving to incorporate: accessibility, reliable applications, and improved user experience.</p>
+            <p className={styles.aboutDesc}>After years of working in the Film Industry, I am making a major career shift into what I am most passionate about. I enjoy the idea that I can create anything from the tips of my fingers. Continuously learning in-demand skills and overcoming unique challenges will help me reach my goal of reinventing what could be possible in Tech.</p>
         </div>
     );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import styles from './Projects.module.css';
 import felp from './img/felp.png';
-import calculator from './img/calculator.png';
 import tax from './img/tax_estimator.png';
 import hospital from './img/hospital.png';
+import maya from './img/maya.png';
+import 'aos/dist/aos.css';
 
 function Projects() {
     return (
         <div className={styles.projectComponentContainer} id='projects'>
             <h1 className={styles.headerOne}>PROJECTS</h1>
-            {/*<div className={`${styles.projectContainer} ${styles.project4}`} data-aos='flip-left' data-aos-once='true'>
-                <div className={styles.projectImgContainer}><img src={expense} alt='Expense tracker application state' className={styles.projectImg} /></div>
-                <div className={styles.project}>
-                    <h2 className={styles.projectHeader}>Expense Tracker</h2>
-                    <p className={`${styles.projectDesc} ${styles.projectFourDesc}`}>This application is a basic expense tracker to keep tabs on your spending.  You can create, read, update, and delete any post that you input.  The backend database is hosted on Heroku and uses Django, Python, and the Django Rest Framework.  The frontend is created using React Native and Typescript.  Follow the viewing instructions on GitHub to view and use the application.</p>
-                    <div className={styles.projectLinkContainer3}>
-                        <a href='https://expo.dev/@leejoonli/expense-tracker' target='_blank' rel='noreferrer' className={styles.projectLink}>Application</a>
-                        <a href='https://github.com/leejoonli/expense-tracker' target='_blank' rel='noreferrer' className={styles.projectLink}>Frontend</a>
-                        <a href='https://github.com/leejoonli/expense-tracker-backend' target='_blank' rel='noreferrer' className={styles.projectLink}>Backend</a>
-                    </div>
-                </div>
-            </div>*/}
             <div className={styles.projectContainer} data-aos='flip-left' data-aos-once='true'>
                 <div className={styles.projectImgContainer}>
                     <img src={felp} alt='Application State' className={styles.projectImg} />
@@ -29,7 +18,6 @@ function Projects() {
                     <h2 className={styles.projectHeader}>FELP</h2>
                     <p className={`${styles.projectDesc} ${styles.projectThreeDesc}`}>FELP is a blog style food review application to document the hottest spots to go to eat.  The application has full CRUD functionaliy for registered users.  Frontend created using React, Javascript, CSS, with requests handled using Axios.  Backend created with MongoDB, Mongoose, Express, with user authentication being handled by Passport.</p>
                     <div className={styles.projectLinkContainer3}>
-                        {/*<a href='https://felp-official.netlify.app/' target='_blank' rel='noreferrer' className={styles.projectLink}>Application</a>*/}
                         <a href='https://github.com/leejoonli/felp-frontend' target='_blank' rel='noreferrer' className={styles.projectLink}>Frontend</a>
                         <a href='https://github.com/leejoonli/felp-backend' target='_blank' rel='noreferrer' className={styles.projectLink}>Backend</a>
                     </div>
@@ -55,21 +43,20 @@ function Projects() {
                     <h2 className={styles.projectHeader}>Hospital Database</h2>
                     <p className={styles.projectDesc}>MVC application of a hospital database created using Python, PostgreSQL, and Django.  Capable of full CRUD functionality and implemented Django Rest Framework.</p>
                     <div className={styles.projectLinkContainer}>
-                        {/*<a href='https://radiant-headland-38660.herokuapp.com/employees/' target='_blank' rel='noreferrer' className={styles.projectLink}>Application</a>*/}
                         <a href='https://github.com/leejoonli/Hospital-Database-ER' target='_blank' rel='noreferrer' className={styles.projectLink}>Repository</a>
                     </div>
                 </div>
             </div>
             <div className={styles.projectContainer} data-aos='flip-left' data-aos-once='true'>
                 <div className={styles.project}>
-                    <h2 className={styles.projectHeader}>Calculator</h2>
-                    <p className={styles.projectDesc}>Calculator application create using C#, Microsoft Visual Studio's Windows Form Template as a base, and deployed on GitHub with an executable file that can be installed on any computer.</p>
+                    <h2 className={styles.projectHeader}>Maya Pizza Plugin</h2>
+                    <p className={styles.projectDesc}>Maya Plugin that opens up Domino's website.  Created using CMake, Maya API, and C++.</p>
                     <div className={styles.projectLinkContainer}>
-                        <a href='https://github.com/leejoonli/calculator' target='_blank' rel='noreferrer' className={styles.projectLink}>Repository</a>
+                        <a href='https://github.com/leejoonli/pizza-api-cpp' target='_blank' rel='noreferrer' className={styles.projectLink}>Repository</a>
                     </div>
                 </div>
                 <div className={styles.projectImgContainer}>
-                    <img src={calculator} alt='Application State' className={styles.projectImg} />
+                    <img src={maya} alt='Application State' className={styles.projectImg} />
                 </div>
             </div>
         </div>
